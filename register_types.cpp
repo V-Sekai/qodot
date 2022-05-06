d204020490fbaef2352d65bca38e1a476b1c6f24
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-05 20:41:22 -0700

    Update register module.

--- register_types.cpp
+++ register_types.cpp
@@ -1,9 +1,16 @@
 #include "qodot.h"
 
-void register_qodot_types() {
-    GDREGISTER_CLASS(Qodot);
+#include "register_types.h"
+
+void initialize_qodot_module(GDNativeInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	GDREGISTER_CLASS(Qodot);
 }
 
-void unregister_qodot_types() {
-
+void uninitialize_qodot_module(GDNativeInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 }
--- register_types.h
+++ register_types.h
@@ -1,3 +1,6 @@
-// Yes, the word in the middle must be the same as the module folder name
-void register_qodot_types();
-void unregister_qodot_types();
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_qodot_module(ModuleInitializationLevel p_level);
+void uninitialize_qodot_module(ModuleInitializationLevel p_level);
